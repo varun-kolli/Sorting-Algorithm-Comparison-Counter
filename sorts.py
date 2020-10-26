58bd79b949532db0861dfc90d6b3a263e801d49b
varun-kolli <vkolli@calpoly.edu>
2020-10-26 00:16:26 -0700

    prayers up

--- sorts.py
+++ sorts.py
@@ -2,22 +2,46 @@ import random
 import time
 
 def selection_sort(list):
-    pass
-    
+
+    count = 0
+    length = len(list)
+    for i in range(0, length):
+        #count += 1
+        min = i
+        for j in range(i+1, length):
+            count += 1
+            if list[j] < list[min]:
+                min = j
+        list[i], list[min] = list[min], list[i]
+    return count
+
+
+
 def insertion_sort(list):
-    pass
-   
+    count = 0
+    for x in range(1, len(list)):
+        temp = list[x]
+        y = x - 1
+
+        while y >= 0 and temp < list[y]:
+            list[y + 1] = list[y]
+            y -= 1
+            count += 1
+        list[y + 1] = temp
+        #count += 1
+    return count
+
+
 
 def main():
     # Code coverage NOT required for main
     # Give the random number generator a seed, so the same sequence of 
     # random numbers is generated at each run
-    random.seed(1234) 
-    
+    random.seed(1234)
     # Generate 5000 random numbers from 0 to 999,999
-    randoms = random.sample(range(1000000), 5000)
+    randoms = random.sample(range(1000000), 32000)
     start_time = time.time() 
-    comps = selection_sort(randoms)
+    comps = insertion_sort(randoms)
     stop_time = time.time()
     print(comps, stop_time - start_time)
 
--- sorts_tests.py
+++ sorts_tests.py
@@ -3,11 +3,24 @@ from sorts import *
 
 class TestLab4(unittest.TestCase):
 
-    def test_simple(self):
+    def test_sel(self):
         nums = [23, 10]
         comps = selection_sort(nums)
         self.assertEqual(comps, 1)
         self.assertEqual(nums, [10, 23])
+
+    def test_sel_01(self):
+        nums = [0]*1000
+        comps = selection_sort(nums)
+        self.assertEqual(comps, 499500)
+        self.assertEqual(nums, [0]*1000)
+
+    def test_ins(self):
+        nums = [23, 10]
+        comps = insertion_sort(nums)
+        self.assertEqual(comps, 1)
+        self.assertEqual(nums, [10, 23])
+
 
 if __name__ == '__main__': 
     unittest.main()
